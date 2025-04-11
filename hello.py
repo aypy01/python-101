16167872434b9adb552ea325bc87310263c512ca
Aditya Yadav <141349437+Adityeah18@users.noreply.github.com>
2025-04-11 15:18:15 +0530

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,16 @@
+# Printing Hello World
+print("Hello World")
+# Then tab terminal
+# Then type your directory with name of your python file have '.py' extaention
+# 1. Hello Wizarding World
+# ------------------------------------------
+print("Welcome to Hogwarts, young wizard!")  # A magical greeting
+# Program 2
+# ------------------------------------------
+# 2. Drawing a Magic Wand with ASCII Art
+# ------------------------------------------
+print("    *")  # Wand tip
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")  # Magic wand or staff shape
